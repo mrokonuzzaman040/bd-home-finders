18bcda308ef0d46cd056e58df4cf3c173ba6fe7a
mrokonuzzaman040 <19201103040@cse.bubt.edu.bd>
2023-11-24 02:04:23 +0600

    Add react-query and axios dependencies

---
+++ src/Routers/UsersRoutes/AdminRouts.jsx
@@ -0,0 +1,28 @@
+import { Navigate, useLocation } from "react-router-dom";
+import useAuth from "../../Components/Hooks/useAuth";
+import useAdmin from "../../Components/Hooks/useAdmin";
+
+
+
+const AdminRoute = ({ children }) => {
+    const { user, loading } = useAuth();
+    const [isAdmin, isAdminLoading] = useAdmin();
+    const location = useLocation();
+
+    if (loading || isAdminLoading) {
+        return <div className="flex items-center justify-center h-screen">
+            <div className="spinner-border text-primary" role="status">
+                <span className="loading loading-ring loading-lg"></span>
+            </div>
+        </div>
+    }
+
+    if (user && isAdmin) {
+        return children;
+    }
+
+    return <Navigate to="/" state={{ from: location }} replace></Navigate>
+
+};
+
+export default AdminRoute;
---
+++ src/Routers/UsersRoutes/PrivetRoutes.jsx
@@ -0,0 +1,24 @@
+import { Navigate, useLocation } from "react-router";
+import useAuth from "../../Components/Hooks/useAuth";
+
+
+
+const PrivateRoute = ({ children }) => {
+    const { user, loading } = useAuth();
+    const location = useLocation();
+
+    if (loading) {
+        return <div className="flex items-center justify-center h-screen">
+            <div className="spinner-border text-primary" role="status">
+                <span className="loading loading-ring loading-lg"></span>
+            </div>
+        </div>
+    }
+
+    if (user) {
+        return children;
+    }
+    return <Navigate to="/login" state={{ from: location }} replace></Navigate>
+};
+
+export default PrivateRoute;
