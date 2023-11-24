f7036fb361b350357e951a0f4e651b8f466de9a1
mrokonuzzaman040 <19201103040@cse.bubt.edu.bd>
2023-11-24 20:17:18 +0600

    Add react-simple-captcha and sweetalert2 dependencies, and remove unused code

--- src/Components/Providers/AuthProvider.jsx
+++ src/Components/Providers/AuthProvider.jsx
@@ -47,14 +47,14 @@ const AuthProvider = ({ children }) => {
             setUser(currentUser);
             if (currentUser) {
                 // get token and store client
-                const userInfo = { email: currentUser.email };
-                axiosPublic.post('/jwt', userInfo)
-                    .then(res => {
-                        if (res.data.token) {
-                            localStorage.setItem('access-token', res.data.token);
-                            setLoading(false);
-                        }
-                    })
+                // const userInfo = { email: currentUser.email };
+                // axiosPublic.post('/jwt', userInfo)
+                //     .then(res => {
+                //         if (res.data.token) {
+                //             localStorage.setItem('access-token', res.data.token);
+                //             setLoading(false);
+                //         }
+                //     })
             }
             else {
                 // TODO: remove token (if token stored in the client side: Local storage, caching, in memory)
