a04a373dd398bc240a72270536bca4b3ec3ea85a
mrokonuzzaman040 <19201103040@cse.bubt.edu.bd>
2023-11-25 22:30:09 +0600

    Add new icons and implement wishlist functionality

--- src/Components/Advertisement/AdsDetails/AdsDetails.jsx
+++ src/Components/Advertisement/AdsDetails/AdsDetails.jsx
@@ -5,8 +5,15 @@ import { GoUnverified } from "react-icons/go";
 import { FaHeartCirclePlus } from "react-icons/fa6";
 import { FaMapMarkerAlt } from "react-icons/fa";
 import { LuBadgeDollarSign } from "react-icons/lu";
-import { FaChartArea } from "react-icons/fa";
-import { CgUnavailable } from "react-icons/cg";
+import { BsFillHouseHeartFill } from "react-icons/bs";
+import { MdPerson } from "react-icons/md";
+import { GiHouse } from "react-icons/gi";
+import { FaBath, FaBed, FaCar } from "react-icons/fa";
+import { MdOutlineLandscape } from "react-icons/md";
+import { TbResize } from "react-icons/tb";
+import useAuth from '../../Hooks/useAuth';
+
+const { user } = useAuth();
 
 const AdsDetails = () => {
 
@@ -31,6 +38,26 @@ const AdsDetails = () => {
     } = useLoaderData();
 
 
+    const handelWishClick = (id) => {
+        const wishData = {
+            home_id: id,
+            home_name,
+            home_location,
+            home_price,
+            home_photo,
+            home_owner_name,
+            home_owner_photo,
+            home_owner_email,
+            home_status,
+
+            username: user.displayName,
+            emai: user.email,
+            photo: user.photoURL,
+        }
+
+        
+    }
+
     return (
         <div className="flex flex-col gap-4">
             <div className="flex items-center justify-center p-4">
@@ -38,14 +65,16 @@ const AdsDetails = () => {
             </div>
             <div className="flex bg-indigo-300 p-10 justify-between items-center">
                 <div className=" flex flex-col justify-start gap-2">
+                    <h2 className='text-xl flex items-center gap-2'><BsFillHouseHeartFill ></BsFillHouseHeartFill>House Name: {home_name}</h2>
                     <h2 className='text-xl flex items-center gap-2'> <FaMapMarkerAlt /> Location: {home_location}</h2>
                     <p className='text-xl flex items-center gap-2 font-bold'><LuBadgeDollarSign /> Price Range: {home_price}</p>
                     <p className='checkbox-primary'>{
                         home_status === "verified" ? <p className='flex items-center justify-start gap-1'><MdOutlineVerified className='text-green-700' />Verified</p> : <p className='flex items-center justify-start gap-1'> <GoUnverified className='text-red-700' />Unverified</p>
                     }</p>
                 </div>
-                <div className="">
-                    <button className='btn text-white border-none bg-indigo-500 shadow-lg shadow-indigo-500/50'><FaHeartCirclePlus /> Add to wishlist</button>
+                <div className="flex flex-col gap-3">
+                    <button onClick={() => { handelWishClick(_id) }} className='btn text-white border-none bg-indigo-500 shadow-lg shadow-indigo-500/50'><FaHeartCirclePlus /> Add to wishlist</button>
+                    <h2 className='flex justify-center items-center btn'><MdPerson ></MdPerson> Agent Name: {home_owner_name}</h2>
                 </div>
             </div>
 
@@ -59,65 +88,35 @@ const AdsDetails = () => {
             </div>
             <div className="grid grid-cols-2 justify-center bg-indigo-300 p-10">
                 <div className="">
-                    <h2 className='text-2xl text-black'>Open House</h2>
-                    <div className="">
-                        <p className='text-gray-700'>
-                            {home_size}
+                    <h2 className='text-2xl text-black'>House Type</h2>
+                    <div className="uppercase text-white">
+                        <p className='text-gray-700 flex items-center gap-2'>
+                            <GiHouse></GiHouse> Type : {home_type}
                         </p>
+                        <p className='text-gray-700 flex items-center gap-2'>
+                            <MdOutlineLandscape ></MdOutlineLandscape> Area : {home_area}
+                        </p>
+                        <p className='text-gray-700 flex items-center gap-2'>
+                            <TbResize></TbResize> PROPERTY SIZE : {home_size}
+                        </p>
+
                     </div>
                 </div>
                 <div className="items-center">
                     <h2 className='text-2xl text-black'>Home Highlights</h2>
                     <div className="">
-                        <p className='text-gray-700'>
-                            {home_type}
+                        <p className='text-gray-700 flex items-center gap-2'>
+                            <FaBed></FaBed> Bed : {home_bed}
+                        </p>
+                        <p className='text-gray-700 flex items-center gap-2'>
+                            <FaBath></FaBath> Bath : {home_bath}
+                        </p>
+                        <p className='text-gray-700 flex items-center gap-2'>
+                            <FaCar></FaCar> GARAGE : {home_garage}
                         </p>
                     </div>
                 </div>
             </div>
-
-            {/* <div className=" bg-indigo-300 p-10">
-                <h2 className='text-2xl text-black'>Home Details</h2>
-                <div className="">
-                    <p className='text-gray-700'>
-                        {
-                            floor_space ? <p className='flex items-center gap-1'><FaChartArea className='text-green-700' />Floor Space: {floor_space}</p> : <p className='flex items-center gap-1'> <CgUnavailable className='text-red-700' />Floor Space: {floor_space}</p>
-                        }
-                    </p>
-                    <p className='text-gray-700'>
-                        {
-                            lot_size ? <p className='flex items-center gap-1'><MdOutlineVerified className='text-green-700' />Lot Size: {lot_size}</p> : <p className='flex items-center gap-1'> <CgUnavailable className='text-red-700' />Lot Size: {lot_size}</p>
-                        }
-                    </p>
-                    <p className='text-gray-700'>
-                        {
-                            year_built ? <p className='flex items-center gap-1'><MdOutlineVerified className='text-green-700' />Year Built: {year_built}</p> : <p className='flex items-center gap-1'> <CgUnavailable className='text-red-700' />Year Built: {year_built}</p>
-                        }
-                    </p>
-                </div>
-            </div> */}
-            {/* <div className="grid grid-cols-2 justify-center bg-indigo-300 p-10">
-                <div className="">
-                    <h2 className='text-2xl text-black'>Home Facilities</h2>
-                    <div className="">
-                        {
-                            home_facility?.map(facility => <p className='text-gray-700'>
-                                <p className='flex items-center gap-1'><MdOutlineVerified className='text-green-700' />{facility}</p>
-                            </p>)
-                        }
-                    </div>
-                </div>
-                <div className="items-center">
-                    <h2 className='text-2xl text-black'>Home Highlights</h2>
-                    <div className="flex gap-3">
-                        <p className='text-gray-700'>
-                            {home_highlights.map(highlight => <p className='flex items-center gap-1'><MdOutlineVerified className='text-green-700' />{highlight}</p>)
-                            }
-                        </p>
-                    </div>
-                </div>
-
-            </div> */}
         </div>
     );
 };
--- src/Components/Hooks/useSecureApi.jsx
+++ src/Components/Hooks/useSecureApi.jsx
@@ -9,14 +9,12 @@ const useSecureApi = () => {
     const navigate = useNavigate();
     const { logOut } = useAuth();
 
-    // request interceptor to add authorization header for every secure call to teh api
+
     axiosSecure.interceptors.request.use(function (config) {
         const token = localStorage.getItem('access-token')
-        // console.log('request stopped by interceptors', token)
         config.headers.authorization = `Bearer ${token}`;
         return config;
     }, function (error) {
-        // Do something with request error
         return Promise.reject(error);
     });
 
@@ -24,16 +22,16 @@ const useSecureApi = () => {
     // intercepts 401 and 403 status
     axiosSecure.interceptors.response.use(function (response) {
         return response;
-    }, async (error) => {
-        const status = error.response.status;
-        // console.log('status error in the interceptor', status);
-        // for 401 or 403 logout the user and move the user to the login
-        if (status === 401 || status === 403) {
-            await logOut();
-            navigate('/login');
-        }
-        return Promise.reject(error);
-    })
+    },
+        async (error) => {
+            const status = error.response.status;
+
+            if (status === 401 || status === 403) {
+                await logOut();
+                navigate('/login');
+            }
+            return Promise.reject(error);
+        })
 
 
     return axiosSecure;
